c026bdebf67eaa8fc36312dc74289b61c5892c11
BonnieLo-wiwynn <bonnie_lo@wiwynn.com>
2025-02-18 22:06:30 +1000

    Start Conditional

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+# Assignment: Write a program that prints one line other than 'hello world', then take two screen shots and upload them below.
+# Date: 1/21/2025
+# Name: Bonnie Lo
+
+print("I am a computer science major at National Taiwan Normal University.");
+
+print("This is my first time writing a program in Python.");
+
+print("I am excited to learn more about programming.");
